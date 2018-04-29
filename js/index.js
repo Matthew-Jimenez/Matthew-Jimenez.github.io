2bc60a827b01b4c0399e9eea7fd6898d26195704
Matt Jimenez <mattjimenez091@gmail.com>
2018-04-28 18:54:28 -0700

    created image walker class, to keep code dry

--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
-// FIXME: Perfect case to use a class, since we are going to do this atleast 3 times
+
+// ImageWalker class walks takes an image tag from the dom
+// and a list of images paths. The result is an image tag
+// that changes images every 2 seconds
+class ImageWalker {
+    constructor(elementId, imagePaths) {
+        this.elementId = document.getElementById(elementId);
+        this.imagePaths = imagePaths; // array of image paths relevant to image tag
+    }
+
+    // begin walking through images
+    start(){
+        const self = this;
+
+
+        // start the count at 1 initially because we are already
+        // on the first image when the page loads up
+        let count = 1;
+
+        // start interval to loop over images every two seconds
+        setInterval(function () {
+            console.log(self.elementId);
+            console.log(self.imagePaths);
+            self.elementId.src = self.imagePaths[count];
+            count++;
+
+            // if we have reached the last image, return to the
+            // first image in the array and start all over
+            if(count > self.imagePaths.length - 1 ){
+                count = 0;
+            }
+
+        }, 2000);
+    }
+
+}
+
+
 
 // image paths to loop over
 const projectOneImages = [
@@ -8,22 +45,10 @@ const projectOneImages = [
 ];
 
 // grab the image html element
-const projectOne = document.getElementById('project-1');
+const imageWalkerOne = new ImageWalker('project-1', projectOneImages);
 
-// start the count at 1 initially because we are already
-// on the first image when the page loads up
-let count = 1;
+imageWalkerOne.start();
 
-// start interval to loop over images every two seconds
-setInterval(function () {
-    projectOne.src = projectOneImages[count];
-    count++;
 
-    // if we have reached the last image, return to the
-    // first image in the array and start all over
-    if(count > projectOneImages.length - 1 ){
-        count = 0;
-    }
 
-}, 2000);
 
